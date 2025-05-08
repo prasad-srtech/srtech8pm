3566bd0e98beb5c28815c09398be0e152f3731f3
root <root@ip-172-31-9-202.ap-south-1.compute.internal>
2025-05-08 15:14:56 +0000

    added numbers.java

--- numbers.java
+++ numbers.java
@@ -1,6 +1,15 @@
-kljjkljkljlkjkljlkj
-dfdfdfdf
-dfdfdf
-dfdf
+Public class login
+{
+   public static void main()
+   {
+       int i =0;
 
-dfdfdfdf
+       for (i=0;i<==35;i++
+
+
+
+       {
+            System.out Println ("Number: "+i);
+       }
+   }
+}
